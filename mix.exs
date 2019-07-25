fb3632923b21dc151bc54e3740dae1e08c6788e4
Rodrigo Nonose <rodrigo.nonose89@gmail.com>
2019-07-25 19:34:56 -0300

    feat(boilerplate): add baseline dependencies

--- mix.exs
+++ mix.exs
@@ -1,29 +1,33 @@
-defmodule ElixirMicroserviceBase.MixProject do
+defmodule ElixirMicroserviceBase.Umbrella.Mixfile do
   use Mix.Project
 
   def project do
     [
-      app: :elixir_microservice_base,
-      version: "0.1.0",
-      elixir: "~> 1.8",
+      apps_path: "apps",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [coveralls: :test]
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
-  def application do
-    [
-      extra_applications: [:logger],
-      mod: {ElixirMicroserviceBase.Application, []}
-    ]
-  end
-
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:timber, "~> 3.1"},
+      {:timber_plug, "~> 1.0"},
+      {:excoveralls, "~> 0.10", only: :test},
+      {:junit_formatter, "~> 3.0", only: :test, runtime: false},
+      {:prometheus_ex, "~> 3.0"},
+      {:prometheus_plugs, "~> 1.1"},
+      {:prometheus_process_collector, "~> 1.0"},
+      {:jason, "~> 1.1"},
+      {:uuid, "~> 1.1"},
+      {:sentry, "~> 7.0"},
+      {:plug_cowboy, "~> 2.0"},
+      {:plug_checkup, "~> 0.5"},
+      {:httpoison, "~> 1.3"},
+      {:telemetry, "~> 0.4"}
     ]
   end
 end
